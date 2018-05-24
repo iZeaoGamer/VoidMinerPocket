91d426ab546e070657985a475a1ec2859664fec5
TheFixerDevelopment <30442910+TheFixerDevelopment@users.noreply.github.com>
2018-05-24 11:34:18 +0100

    Fixed Experience0rb
    Since this is already implemented into Pocketmine, there's no need to add TeaSpoon's version of it. So I have reverted this change.

--- src/pocketmine/entity/object/ExperienceOrb.php
+++ src/pocketmine/entity/object/ExperienceOrb.php
@@ -1,117 +1,180 @@
 <?php
 
-/**
+/*
  *
- * MMP""MM""YMM               .M"""bgd
- * P'   MM   `7              ,MI    "Y
- *      MM  .gP"Ya   ,6"Yb.  `MMb.   `7MMpdMAo.  ,pW"Wq.   ,pW"Wq.`7MMpMMMb.
- *      MM ,M'   Yb 8)   MM    `YMMNq. MM   `Wb 6W'   `Wb 6W'   `Wb MM    MM
- *      MM 8M""""""  ,pm9MM  .     `MM MM    M8 8M     M8 8M     M8 MM    MM
- *      MM YM.    , 8M   MM  Mb     dM MM   ,AP YA.   ,A9 YA.   ,A9 MM    MM
- *    .JMML.`Mbmmd' `Moo9^Yo.P"Ybmmd"  MMbmmd'   `Ybmd9'   `Ybmd9'.JMML  JMML.
- *                                     MM
- *                                   .JMML.
- * This file is part of TeaSpoon.
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
  *
- * TeaSpoon is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * TeaSpoon is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
+ * @author PocketMine Team
+ * @link http://www.pocketmine.net/
  *
- * You should have received a copy of the GNU Affero General Public License
- * along with TeaSpoon.  If not, see <http://www.gnu.org/licenses/>.
  *
- * @author CortexPE
- * @link https://CortexPE.xyz
- *
- */
+*/
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
-namespace CortexPE\entity\object;
+namespace pocketmine\entity\object;
 
-use CortexPE\Main;
+use pocketmine\entity\Entity;
 use pocketmine\entity\Human;
-use pocketmine\entity\object\ExperienceOrb as PMXPOrb;
-use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\nbt\tag\IntTag;
+use pocketmine\nbt\tag\ShortTag;
 use pocketmine\Player;
 
-class ExperienceOrb extends PMXPOrb {
-	/** @var Human */
-	protected $targetPlayer = null;
+class ExperienceOrb extends Entity{
+	public const NETWORK_ID = self::XP_ORB;
 
-	public function hasTargetPlayer() : bool{
-		return $this->targetPlayer instanceof Human;
-	}
+	public const TAG_VALUE_PC = "Value"; //short
+	public const TAG_VALUE_PE = "experience value"; //int (WTF?)
 
-	public function getTargetPlayer() : ?Human{
-		return $this->targetPlayer;
-	}
+	/**
+	 * Max distance an orb will follow a player across.
+	 */
+	public const MAX_TARGET_DISTANCE = 8.0;
 
-	public function setTargetPlayer(?Human $player) : void{
-		$this->targetPlayer = $player;
-	}
+	/**
+	 * Split sizes used for dropping experience orbs.
+	 */
+	public const ORB_SPLIT_SIZES = [2477, 1237, 617, 307, 149, 73, 37, 17, 7, 3, 1]; //This is indexed biggest to smallest so that we can return as soon as we found the biggest value.
 
-	public function entityBaseTick(int $tickDiff = 1): bool{
-		// Code below was taken from PMMP's Entity class (grandparent of this class) so that it still ticks normally...
-		$this->justCreated = \false;
-
-		$changedProperties = $this->propertyManager->getDirty();
-		if(!empty($changedProperties)){
-			$this->sendData($this->hasSpawned, $changedProperties);
-			$this->propertyManager->clearDirtyProperties();
-		}
-
-		$hasUpdate = \false;
-
-		$this->checkBlockCollision();
-
-		if($this->y <= -16 and $this->isAlive()){
-			$ev = new EntityDamageEvent($this, EntityDamageEvent::CAUSE_VOID, 10);
-			$this->attack($ev);
-			$hasUpdate = \true;
-		}
-
-		if($this->isOnFire()){
-			$hasUpdate = ($hasUpdate || $this->doOnFireTick($tickDiff));
-		}
-
-		if($this->noDamageTicks > 0){
-			$this->noDamageTicks -= $tickDiff;
-			if($this->noDamageTicks < 0){
-				$this->noDamageTicks = 0;
+	/**
+	 * Returns the largest size of normal XP orb that will be spawned for the specified amount of XP. Used to split XP
+	 * up into multiple orbs when an amount of XP is dropped.
+	 *
+	 * @param int $amount
+	 *
+	 * @return int
+	 */
+	public static function getMaxOrbSize(int $amount) : int{
+		foreach(self::ORB_SPLIT_SIZES as $split){
+			if($amount >= $split){
+				return $split;
 			}
 		}
 
-		$this->age += $tickDiff;
-		$this->ticksLived += $tickDiff;
+		return 1;
+	}
 
+	/**
+	 * Splits the specified amount of XP into an array of acceptable XP orb sizes.
+	 *
+	 * @param int $amount
+	 *
+	 * @return int[]
+	 */
+	public static function splitIntoOrbSizes(int $amount) : array{
+		$result = [];
 
-		/////////////////////////////////////////// real thing /////////////////////////////////////////////////////////
-		if($this->isClosed()){
-			return true;
+		while($amount > 0){
+			$size = self::getMaxOrbSize($amount);
+			$result[] = $size;
+			$amount -= $size;
 		}
-		if($this->age > Main::$XPTicksTillDespawn){
-			$this->close();
+
+		return $result;
+	}
+
+	public $height = 0.25;
+	public $width = 0.25;
+
+	public $gravity = 0.04;
+	public $drag = 0.02;
+
+	/**
+	 * @var int
+	 * Ticker used for determining interval in which to look for new target players.
+	 */
+	protected $lookForTargetTime = 0;
+
+	/**
+	 * @var int|null
+	 * Runtime entity ID of the player this XP orb is targeting.
+	 */
+	protected $targetPlayerRuntimeId = null;
+
+	protected function initEntity() : void{
+		parent::initEntity();
+
+		$this->age = $this->namedtag->getShort("Age", 0);
+
+		$value = 0;
+		if($this->namedtag->hasTag(self::TAG_VALUE_PC, ShortTag::class)){ //PC
+			$value = $this->namedtag->getShort(self::TAG_VALUE_PC);
+		}elseif($this->namedtag->hasTag(self::TAG_VALUE_PE, IntTag::class)){ //PE save format
+			$value = $this->namedtag->getInt(self::TAG_VALUE_PE);
+		}
+
+		$this->setXpValue($value);
+	}
+
+	public function saveNBT() : void{
+		parent::saveNBT();
+
+		$this->namedtag->setShort("Age", $this->age);
+
+		$this->namedtag->setShort(self::TAG_VALUE_PC, $this->getXpValue());
+		$this->namedtag->setInt(self::TAG_VALUE_PE, $this->getXpValue());
+	}
+
+	public function getXpValue() : int{
+		return $this->propertyManager->getInt(self::DATA_EXPERIENCE_VALUE) ?? 0;
+	}
+
+	public function setXpValue(int $amount) : void{
+		if($amount <= 0){
+			throw new \InvalidArgumentException("XP amount must be greater than 0, got $amount");
+		}
+		$this->propertyManager->setInt(self::DATA_EXPERIENCE_VALUE, $amount);
+	}
+
+	public function hasTargetPlayer() : bool{
+		return $this->targetPlayerRuntimeId !== null;
+	}
+
+	public function getTargetPlayer() : ?Human{
+		if($this->targetPlayerRuntimeId === null){
+			return null;
+		}
+
+		$entity = $this->server->findEntity($this->targetPlayerRuntimeId, $this->level);
+		if($entity instanceof Human){
+			return $entity;
+		}
+
+		return null;
+	}
+
+	public function setTargetPlayer(?Human $player) : void{
+		$this->targetPlayerRuntimeId = $player ? $player->getId() : null;
+	}
+
+	public function entityBaseTick(int $tickDiff = 1) : bool{
+		$hasUpdate = parent::entityBaseTick($tickDiff);
+
+		if($this->age > 6000){
+			$this->flagForDespawn();
 			return true;
 		}
 
 		$currentTarget = $this->getTargetPlayer();
+		if($currentTarget !== null and $currentTarget->distanceSquared($this) > self::MAX_TARGET_DISTANCE ** 2){
+			$currentTarget = null;
+		}
 
 		if($this->lookForTargetTime >= 20){
-			if($currentTarget === null or $currentTarget->distanceSquared($this) > self::MAX_TARGET_DISTANCE ** 2){
-				$this->setTargetPlayer(null);
-
+			if($currentTarget === null){
 				$newTarget = $this->level->getNearestEntity($this, self::MAX_TARGET_DISTANCE, Human::class);
 
 				if($newTarget instanceof Human and !($newTarget instanceof Player and $newTarget->isSpectator())){
 					$currentTarget = $newTarget;
-					$this->setTargetPlayer($currentTarget);
 				}
 			}
 
@@ -119,6 +182,8 @@ class ExperienceOrb extends PMXPOrb {
 		}else{
 			$this->lookForTargetTime += $tickDiff;
 		}
+
+		$this->setTargetPlayer($currentTarget);
 
 		if($currentTarget !== null){
 			$vector = $currentTarget->subtract($this)->add(0, $currentTarget->getEyeHeight() / 2, 0)->divide(self::MAX_TARGET_DISTANCE);
@@ -132,22 +197,25 @@ class ExperienceOrb extends PMXPOrb {
 				$this->motionZ += $vector->z / $distance * $oneMinusDistance * 0.2;
 			}
 
-			$canPickup = true;
-			if(Main::$XPPickupDelay && !$currentTarget->canPickupXp()){
-				$canPickup = false;
-			}
+			if($currentTarget->canPickupXp() and $this->boundingBox->intersectsWith($currentTarget->getBoundingBox())){
+				$this->flagForDespawn();
 
-			if($canPickup and $this->boundingBox->intersectsWith($currentTarget->getBoundingBox())){
 				$currentTarget->addXp($this->getXpValue());
-				if(Main::$XPPickupDelay){
-					$currentTarget->resetXpCooldown();
-				}
-				$this->close();
+				$currentTarget->resetXpCooldown();
 
 				//TODO: check Mending enchantment
 			}
 		}
 
 		return $hasUpdate;
+	}
+
+	protected function tryChangeMovement() : void{
+		$this->checkObstruction($this->x, $this->y, $this->z);
+		parent::tryChangeMovement();
+	}
+
+	public function canBeCollidedWith() : bool{
+		return false;
 	}
 }
